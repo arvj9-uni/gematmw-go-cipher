331cd6bfafc16b110249f28e6a8369ac75d404c1
Aries Vince Picana <aries_picana@dlsu.edu.ph>
2020-12-27 14:32:37 +0800

    Initial Setup

--- cipher.go
+++ cipher.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/thatisuday/commando"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	commando.
+		SetExecutableName("cipher").
+		SetVersion("1.0.0").
+		SetDescription("This CLI application allows one to encrypt and decrypt messages.")
 }
