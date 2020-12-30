e710fea4088b2d1160fe79ef7ce17ac69af20a28
Aries Vince Picana <aries_picana@dlsu.edu.ph>
2020-12-30 18:53:31 +0800

    Rewritten code

---
+++ cipher.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/thatisuday/commando"
+)
+
+func main() {
+	//	configure commando
+	commando.
+		SetExecutableName("cipher").
+		SetVersion("2.0.0").
+		SetDescription("This is a CLI application that encrypts/decrypts messages using different methods that may or may not have been discussed in class.")
+
+	//	configure the atbash command
+	commando.
+		Register("atbash").
+		AddArgument("message", "message to encrypt/decrypt", "").
+		SetAction(atbashCipher)
+
+	//	configure the shift command
+	commando.
+		Register("shift").
+		AddArgument("message", "message to encrypt/decrypt", "").
+		AddFlag("process,p", "encrypt/decrypt", commando.String, "encrypt").
+		AddFlag("key,k", "cipher shift key", commando.Int, 0).
+		SetAction(shiftCipher)
+
+	//	configure the vigenère command
+	commando.
+		Register("vigenere").
+		AddArgument("message", "message to encrypt/decrypt", "").
+		AddFlag("process,p", "encrypt/decrypt", commando.String, "encrypt").
+		AddFlag("key,k", "cipher key", commando.String, "a").
+		SetAction(vigenereCipher)
+
+	//	configure the rail fence command
+	commando.
+		Register("rail").
+		AddArgument("message", "message to encrypt/decrypt", "").
+		AddFlag("process,p", "encrypt/decrypt", commando.String, "encrypt").
+		AddFlag("key,k", "rail fence cipher key", commando.Int, 0).
+		SetAction(railFenceCipher)
+
+	//	configure the RSA cryptosystem command
+	commando.
+		Register("rsa").
+		AddArgument("message", "message to encrypt/decrypt", "").
+		AddFlag("process,p", "encrypt/decrypt", commando.String, "encrypt").
+		AddFlag("public,e", "RSA cipher public key", commando.Int, 0).
+		AddFlag("private,d", "RSA cipher private key", commando.Int, 0).
+		SetAction(rsaCipher)
+}
+
+func atbashCipher(_ map[string]commando.ArgValue, _ map[string]commando.FlagValue) {}
+
+func shiftCipher(_ map[string]commando.ArgValue, _ map[string]commando.FlagValue) {}
+
+func vigenereCipher(_ map[string]commando.ArgValue, _ map[string]commando.FlagValue) {}
+
+func railFenceCipher(_ map[string]commando.ArgValue, _ map[string]commando.FlagValue) {}
+
+func rsaCipher(_ map[string]commando.ArgValue, _ map[string]commando.FlagValue) {}
